7c9e7a6d05e57d84585096d093bbb4ecfb7a258b
consoleeumuhoze <umuhozesylvain@gmail.com>
2023-11-08 17:00:07 +0300

    started working on auth app

---
+++ products/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+
+def products(request):
+    return render(request, 'products/products.html')
+
+
+def add_products(request):
+    return render(request, 'products/add-products.html')
+
+
+def update_products(request):
+    return render(request, 'products/update-products.html')
+
+
